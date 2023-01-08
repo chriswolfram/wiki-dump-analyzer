cc0daa1a900781a4fbc21e8e78a843f97e8f1f20
Christopher Wolfram <chriscwolfram@gmail.com>
2023-01-08 02:38:41 -0600

    Added description.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 //! High-performance framework for reading and analyzing wiki(pedia) dump files.
 //! # Description
-//!
+//! Periodic dumps of wikipedia are available from [Wikimedia](https://www.wikimedia.org/), an example
+//! of which can be seen [here](https://dumps.wikimedia.org/enwiki/20230101/). This crate is designed
+//! to process the most complete dumps available, containing the complete edit history of all pages on a wiki.
+//! (For this reason, the `.7z` versions of the dumps are used because they are far more efficient.)
+//! ## Dependency
+//! This crate currently assumes that the `7z` command line application is installed and available to be
+//! called with [`std::process::Command::new`].
 
 pub mod dump;
 pub use dump::{Dump, DumpStore, Page, PageIterator, Revision};
